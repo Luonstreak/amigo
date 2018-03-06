2399c4c2987b65e980d289435468545454642389
Lucio Rodriguez <luonstreak@gmail.com>
2018-03-06 13:07:23 -0800

    basic guess component

--- src/actions/GameActions.js
+++ src/actions/GameActions.js
@@ -6,7 +6,6 @@ export const renderCard = (game, status) => {
 	const ref = firebase.database().ref(`games/${game}`);
 	return (dispatch) => {
 		ref.limitToLast(5).once('value', async snap => {
-			console.log(snap.val())
 			await dispatch({ type: FETCH_FIVE, payload: snap.val() })
 			if (status == 'guess') {
 				firebase.database.ref(`result/${game}`)
@@ -19,14 +18,11 @@ export const renderCard = (game, status) => {
 export const fetchQuestion = (id) => {
 	const num = Math.floor(Math.random() * 2) + 1
 	const questionRef = id + num
-	console.log(questionRef)
-	console.log(num)
 	const ref = firebase.database().ref(`questions/${id}/${id}${num}`);
 
 	return (dispatch) => {
 		ref.once('value', async snap => {
 			const obj = { questionNumber: snap.key, content: snap.val().content, choices: snap.val().choices }
-			console.log(obj)
 			await dispatch({ type: QUESTION_CHOSEN, payload: obj })
 			Actions.game()
 		})
@@ -45,8 +41,7 @@ export const saveAnswer = (num, questionId, opponent) => {
 		const option3 = snap.val().choices.option3
 		const option4 = snap.val().choices.option4
 		const content = snap.val().content
-		console.log(content)
-
+		console.log('before game creation')
 		pushId = firebase.database().ref('games').push({
 			[questionId]: {
 				content: content,
@@ -62,7 +57,7 @@ export const saveAnswer = (num, questionId, opponent) => {
 		})
 		const key = pushId.getKey()
 		
-		firebase.database().ref(`result/${key}`).update({
+		firebase.database().ref(`result/${key}`).set({
 			[questionId]: {
 				content: content,
 				choices: {
@@ -89,10 +84,10 @@ export const saveAnswer = (num, questionId, opponent) => {
 				})
 			}
 			else {
-				firebase.database().ref(`users/${currentUser.uid}/games/${key}`).update({
+				firebase.database().ref(`users/${currentUser.uid}/games/${key}`).set({
 					status: 'guess'
 				})
-				firebase.database().ref(`users/${opponent}/games/${key}`).update({
+				firebase.database().ref(`users/${opponent}/games/${key}`).set({
 					status: 'waiting'
 				})
 			}
@@ -100,7 +95,6 @@ export const saveAnswer = (num, questionId, opponent) => {
 	})
 	return (dispatch) => {
 		dispatch({ type: GAME_CREATED })
-		console.log('this is the gameActions')
 		Actions.dashboard()
 	}
 }
